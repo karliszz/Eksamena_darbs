6bf189d74cbc65e7e19666d3aac569d4ca30ed81
Karlis Zeiburlins <karliszeibis@gmail.com>
2021-06-15 12:37:13 +0300

    Izveidots pirmais jautajums ar četriem atbilžu variantiem

--- Tests_Zeiburlins.cpp
+++ Tests_Zeiburlins.cpp
@@ -4,4 +4,10 @@ int main (){
 	int x, pareizs = 0;
 	string atbilde;
 	
+	//1 jautajums
+	cout<<"Ja katram masiva elementam ir tikai viens indekss, kas nodrosina pieeju tam, tad masivu sauc par?\nAtbildi ar a, b, c vai d burtiem."<<endl;
+    cout<<"a) Viendimensiju "<<endl; 
+    cout<<"b) Divdimensiju "<<endl;
+    cout<<"c) Trisdimensiju "<<endl;
+    cout<<"d) Cetrdimensiju "<<endl;
 }
